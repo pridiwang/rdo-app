6d649140c81ed31a2861ccef91addb1f9eb6cf52
pridiwang <preedeew@gmail.com>
2022-11-01 10:30:01 +0700

    work on smile

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import { Video, AVPlaybackStatus } from 'expo-av';
 
 export default function App() {
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Video
+        ref={video}
+        style={styles.video}
+        source={
+          {uri: 'https://simplep-wegfa5.cdn.byteark.com/fleetstream/simplepradio/720p/index.m3u8'}      
+      }
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        shouldPlay
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
+        />
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  video:{
+    flex:1,
+    width:320,
+    height:200,
+  },
+  buttons:{
+    flex:1,
+  }
 });
